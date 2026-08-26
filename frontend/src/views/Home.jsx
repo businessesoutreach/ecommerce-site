834a012705518dc1e125ab4244ac2d5b70369f18
Sami-Sial <samisial1555@gmail.com>
2026-08-26 21:24:09 +0500

    fix: add useState import back to Home.jsx

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import useSWR from "swr";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
